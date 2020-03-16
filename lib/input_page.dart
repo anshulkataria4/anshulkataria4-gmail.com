04494aa83c4241cb270bb6e5c6d972f323d9fd92
Anshul Kataria <flunkeyit@gmail.com>
2020-03-17 02:17:52 +0530

    a new custom widget with constructor

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -18,60 +18,47 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(colour:Color(0xFF1D1E33)),
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(colour:Color(0xFF1D1E33))
                 )
               ],
             ),
           ),
           Expanded(
-            child: Container(
-              margin: EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10),
-              ),
-            ),
+            child: ReusableCard(colour:Color(0xFF1D1E33))
           ),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(colour:Color(0xFF1D1E33))
                 ),
                 Expanded(
-                  child: Container(
-                    margin: EdgeInsets.all(15),
-                    decoration: BoxDecoration(
-                      color: Color(0xFF1D1E33),
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                  ),
+                  child: ReusableCard(colour:Color(0xFF1D1E33))
                 ),
               ],
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  Color colour;
+
+  ReusableCard({@required this.colour});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: Color(0xFF1D1E33),
+        borderRadius: BorderRadius.circular(10),
       ),
     );
   }
